0e2c790717efed724db39f525755d815f1e7e8dc
Yashvanth <yashvanth.vijayabalaji@acxhange.com>
2026-01-08 21:04:39 +0530

    Learnt till Arithmetic & Math

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import math
+
 # First Python Program
 print("I like biriyani")
 print("It's really good")
@@ -72,3 +74,35 @@ age = age + 1
 print(f"Hello {name}")
 print("Happy Bday")
 print(f"Age: {age}")
+print()
+
+
+# arithmetic and math
+x = 3
+y = 2
+z = x**y
+print(f"{x}**{y}={z}")
+
+x = 10
+y = 3
+z = x % y
+print(f"{x}%{y}={z}")
+
+x = 3.14
+y = -5
+z = 5
+print(f"Rounded: {round(x)}")
+print(f"Absolute: {abs(y)}")
+print(f"Power: {pow(2,2)}")
+print(f"Maximum: {max(x,y,z)}")
+print(f"Minimum: {min(x,y,z)}")
+
+x = 9
+y = 9.1
+z = 9.9
+print(f"PI: {math.pi}")
+print(f"Square Root: {math.sqrt(x)}")
+print(f"Ceil: {math.ceil(y)}")
+print(f"Floor: {math.floor(z)}")
+print(f"PI Rounded to two places: {round(math.pi, 2)}")
+print()
